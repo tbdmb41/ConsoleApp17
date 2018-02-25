87b81847b3ff5f1fd0eb41a0760b96091ccda042
tbdmb41 <tbowen1@outlook.com>
2018-02-25 13:18:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "1234";
+            byte b = Convert.ToByte(number);
+            Console.WriteLine(b);
+        }
+    }
+}
